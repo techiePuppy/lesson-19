1d4f526201024e688ee647fa2035a8ea2ece187d
Vishnutej Mamidi <vishnutej.mamidi2@gmail.com>
2025-11-22 13:20:50 +0000

    v1

---
+++ act2.py
@@ -0,0 +1,2 @@
+import math
+print('the floor and celling value of 23,56 are:'+ (math.ceil(23,36))+","+  str(math.floor(23,36))) 
---
+++ hw2.py
@@ -0,0 +1,6 @@
+o=23
+a=54
+h=98
+print("sin=",o/h)
+print("cos=",a/h)
+print("tan=",a/o)
